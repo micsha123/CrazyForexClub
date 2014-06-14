35d18c5bdd04ba81171f80cb6dc2eeb42142986e
micsha123 <micsha123@gmail.com>
2014-06-14 11:27:46 +0700

    SE

---
+++ src/com/example/crazyforexclub/FoolVilage.java
@@ -0,0 +1,51 @@
+package com.example.crazyforexclub;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class FoolVilage extends Service {
+    MediaPlayer player;
+    public IBinder onBind(Intent arg0) {
+
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.kalimba);
+        player.setLooping(true); // Set looping
+        player.setVolume(100,100);
+
+    }
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return 1;
+    }
+
+    public void onStart(Intent intent, int startId) {
+        // TO DO
+    }
+    public IBinder onUnBind(Intent arg0) {
+        // TO DO Auto-generated method
+        return null;
+    }
+
+    public void onStop() {
+
+    }
+    public void onPause() {
+
+    }
+    @Override
+    public void onDestroy() {
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
+}
